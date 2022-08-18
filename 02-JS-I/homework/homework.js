5938d7534bd1dcd8b87d31746f225cca7d88bb21
Matias <akitaotaku@gmail.com>
2022-08-18 09:50:24 -0300

    JSII Terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,7 +232,6 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var euro 
   return euro * 1.20
 }
 
